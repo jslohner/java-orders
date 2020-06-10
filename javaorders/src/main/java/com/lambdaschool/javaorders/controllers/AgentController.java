f01c98aca12d05da3ab9c381d2b1e1f7333af7a3
Justin Lohner <lohner4@gmail.com>
2020-06-09 21:29:24 -0600

    Got /agents/agent/{agentId} endpoint working

--- javaorders/src/main/java/com/lambdaschool/javaorders/controllers/AgentController.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/controllers/AgentController.java
@@ -1,9 +1,25 @@
 package com.lambdaschool.javaorders.controllers;
 
+import com.lambdaschool.javaorders.models.Agent;
+import com.lambdaschool.javaorders.services.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/agents")
 public class AgentController {
+	@Autowired
+	private AgentService agentService;
+
+	// http://localhost:2000/agents/agent/{agentId}
+	@GetMapping(value = "/agent/{agentId}", produces = {"application/json"})
+	public ResponseEntity<?> getAgentById(@PathVariable long agentId) {
+		Agent agent = agentService.findAgentById(agentId);
+		return new ResponseEntity<>(agent, HttpStatus.OK);
+	}
 }
