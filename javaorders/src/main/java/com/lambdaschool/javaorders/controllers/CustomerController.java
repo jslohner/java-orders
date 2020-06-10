d2c154ced133999d257d3e10e9b15fee90e7a301
Justin Lohner <lohner4@gmail.com>
2020-06-09 22:07:40 -0600

    Got all of the mvp /customers endpoints working

--- javaorders/src/main/java/com/lambdaschool/javaorders/controllers/CustomerController.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/controllers/CustomerController.java
@@ -1,9 +1,38 @@
 package com.lambdaschool.javaorders.controllers;
 
+import com.lambdaschool.javaorders.models.Customer;
+import com.lambdaschool.javaorders.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping(value = "/orders", produces = {"application/json"})
+	public ResponseEntity<?> findAllCustomers() {
+		List<Customer> allCustomers = customerService.findAllCustomers();
+		return new ResponseEntity<>(allCustomers, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/customer/{custId}", produces = {"application/json"})
+	public ResponseEntity<?> findCustomerById(@PathVariable long custId) {
+		Customer customer = customerService.findCustomerById(custId);
+		return new ResponseEntity<>(customer, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/namelike/{likename}", produces = {"application/json"})
+	public ResponseEntity<?> listAllCustomersLikeName(@PathVariable String likename) {
+		List<Customer> likeNameCustomers = customerService.findByNameLike(likename);
+		return new ResponseEntity<>(likeNameCustomers, HttpStatus.OK);
+	}
 }
---
+++ javaorders/src/main/java/com/lambdaschool/javaorders/services/CustomerServiceImpl.java
@@ -0,0 +1,38 @@
+package com.lambdaschool.javaorders.services;
+
+import com.lambdaschool.javaorders.models.Customer;
+import com.lambdaschool.javaorders.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service(value = "customerService")
+public class CustomerServiceImpl implements CustomerService {
+	@Autowired
+	private CustomerRepository custrepo;
+
+	@Override
+	public List<Customer> findAllCustomers() {
+		List<Customer> rtnList = new ArrayList<>();
+
+		custrepo.findAll()
+				.iterator()
+				.forEachRemaining(rtnList::add);
+
+		return rtnList;
+	}
+
+	@Override
+	public Customer findCustomerById(long id) {
+		return custrepo.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Customer " + id + " Not Found"));
+	}
+
+	@Override
+	public List<Customer> findByNameLike(String likename) {
+		return custrepo.findByCustnameContainingIgnoringCase(likename);
+	}
+}
